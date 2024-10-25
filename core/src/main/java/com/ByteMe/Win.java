e936f64ef861bb385130833fa5a32a5ab2d2d3ca
Anouska Sahoo <anouska.sahoo007@gmail.com>
2024-10-25 14:30:17 +0530

    win screen

---
+++ core/src/main/java/com/ByteMe/Win.java
@@ -0,0 +1,116 @@
+package com.ByteMe;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Win implements Screen {
+
+    private SpriteBatch batch;
+    private Texture bgtexture;
+    private Texture nextButton;
+    private Texture endButton;
+    private Texture saveButton;
+    private MainLauncher game;
+    private final Player player;
+    private int status;
+
+    public Win(MainLauncher game, int i, Player player) {
+        this.game=game;
+        this.player = player;
+        batch = new SpriteBatch();
+        status = i;
+        bgtexture = new Texture("win_bg.png");
+        nextButton = new Texture("nextlevel_button.png");
+        endButton = new Texture("exitlevel_button.png");
+        saveButton = new Texture("savegame_button.png");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+        Gdx.gl.glClearColor(0.5f, 0.8f, 0.9f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        int resume_x = 150;
+        int resume_y = 180;
+        int resume_width = 500;
+        int resume_height = 60;
+        int save_x = 150;
+        int save_y = 105;
+        int save_width = 240;
+        int save_height = 55;
+        int end_x = 410;
+        int end_y = 105;
+        int end_width = 240;
+        int end_height = 55;
+
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+            float mouseX = Gdx.input.getX();
+            float mouseY = Gdx.graphics.getHeight() - Gdx.input.getY();
+
+            if (mouseX >= resume_x && mouseX <= resume_x+resume_width && mouseY >= resume_y && mouseY <= resume_y+resume_height) {
+                switch (status){
+                    case 0:
+                        game.setScreen(new LoadedGame(game, player));
+                        break;
+                    case 1:
+                        game.setScreen(new Level1(game, player));
+                        break;
+                    case 2:
+                        game.setScreen(new Level2(game, player));
+                        break;
+                    case 3:
+                        //game.setScreen(new Level3(game));
+                        break;
+                }
+            }
+            if (mouseX >= save_x && mouseX <= save_x+save_width && mouseY >= save_y && mouseY <= save_y+save_height) {
+                game.setScreen(new LevelsScreen(game, player));
+            }
+            if (mouseX >= end_x && mouseX <= end_x+end_width && mouseY >= end_y && mouseY <= end_y+end_height) {
+                game.setScreen(new HomeScreen(game, player));
+            }
+
+        }
+
+        batch.begin();
+        batch.draw(bgtexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(nextButton, resume_x, resume_y, resume_width, resume_height);
+        batch.draw(saveButton, save_x, save_y, save_width, save_height);
+        batch.draw(endButton, end_x, end_y, end_width, end_height);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
